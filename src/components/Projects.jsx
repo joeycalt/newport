7738f45633b18e0a299411594cf7c98bb6ce5381
joeycalt <joeycalt@yahoo.com>
2023-05-01 15:56:57 -0400

    fixed projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -63,7 +63,7 @@ const Projects = () => {
             </p>
         </div>
 
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
+        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-4 sm:px-0'>
         {
             projects.map(({id, src, title, skills, live, code}) => (
             <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
