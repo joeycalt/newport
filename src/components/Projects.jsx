48630c16b1b8221b06545d973c3fe236b8b044ca
joeycalt <joeycalt@yahoo.com>
2023-05-02 13:59:54 -0400

    removed projects with bugs 4

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -50,7 +50,7 @@ const Projects = () => {
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-4 sm:px-0'>
         {
             projects.map(({id, src, title, skills, live, code}) => (
-            <div key={id} className='shadow-md shadow-gray-600 rounded-lg w-90'>
+            <div key={id} className='shadow-md shadow-gray-600 rounded-lg w-80'>
                 <img src={src} alt='' className='rounded-md duration-200 hover:scale-105 w-90'/> <p className='mt-4 text-center text-sm'>{title}</p>
                 <div className='flex items-center justify-center'>
                     <a href={live}
